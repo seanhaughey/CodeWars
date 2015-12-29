b87736bc3232352d1743d890088cbff99de16637
Sean Haughey <sdhaughey@gmail.com>
2015-12-29 13:39:36 -0600

    bowling score calculator solution edge case fixed

--- bowling_score_calculator.js
+++ bowling_score_calculator.js
@@ -1,18 +1,26 @@
 var bowlingScore = function(rolls) {
     var score = 0;
+    var frame = 1;
     for (i=0; i < rolls.length; i++) {
       if ((rolls[i] + rolls[i+1]) < 10 ) {
           score = score + rolls[i] + rolls[i+1];
           i++;
+          frame = frame + 1;
       }
-      else if ((rolls[i] !== 10) && ((rolls[i] + rolls[i+1]) === 10)) {
+      else if ((rolls[i] !== 10) && ((rolls[i] + rolls[i+1]) === 10) && (frames < 10)) {
           score = score + 10 + rolls[i+2];
           i++;
+          frame = frame + 1;
       }
-      else if ((i < rolls.length - 3) && (rolls[i] === 10)) {
+      else if ((rolls[i] !== 10) &&((rolls[i] + rolls[i+1]) === 10) && (frames === 10)) {
+          score = score + 10 + rolls[i+2];
+          i = rolls.length;
+      }
+      else if ((frame != 10) && (rolls[i] === 10)) {
           score = score + 10 + rolls[i+1] + rolls[i+2];
+          frame = frame + 1;
       }
-      else if ((i === rolls.length - 3) && (rolls[i] === 10)){
+      else if ((frame === 10) && (rolls[i] === 10)){
           score = score + 10 + rolls[i+1] + rolls[i+2];
           i = rolls.length;
       }
