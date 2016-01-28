8bab3f27e1f67225b209da711e24c28e886f78c6
Sean Haughey <sdhaughey@gmail.com>
2016-01-28 17:06:59 -0600

    ean validation solution

---
+++ ean_validation.js
@@ -0,0 +1,18 @@
+function validateEAN(eanCode) {
+    var array = eanCode.split("");
+    var sum = 0;
+    for(i=0; i<12; i++){
+        if(i%2 !== 0) {
+            array[i] = array[i] * 3;
+        }
+        else {
+            array[i] = array[i] * 1;
+        }
+        sum = sum + array[i];
+    }
+    var checksum = 10 - (sum%10);
+    if(checksum === 10) {
+        checksum = 0;
+    }
+    return checksum === parseInt(array[12]);
+}
