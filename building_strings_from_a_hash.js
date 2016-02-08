aac5ef60b5ba2ce4fc155a40f5bca0ba355048e0
Sean Haughey <sdhaughey@gmail.com>
2016-02-08 17:29:44 -0600

    removed console log

--- building_strings_from_a_hash.js
+++ building_strings_from_a_hash.js
@@ -1,7 +1,6 @@
 function solution(pairs){
     var k = Object.keys(pairs);
     var solution = "";
-    console.log(k);
     for(i=0; i<k.length; i++){
         if(i==k.length-1) {
             solution = solution + k[i] + " = " + pairs[k[i]];
