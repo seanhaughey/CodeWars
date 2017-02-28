5c3ce65b832489ca2f3925da28a0b5d5cf178074
Billakurthi <avinash.billakurthi@gmail.com>
2017-02-28 19:56:55 +0530

    updated code to check for NaN Issue

--- get_the_middle_character.js
+++ get_the_middle_character.js
@@ -1,3 +1,3 @@
 function getMiddle(s){
-	return	s.length%2===0 ? s[s.length/2-1] + s[s.length/2]:s[Math.floor(s.length/2)];
+	return s == "NaN" ? "" :  (s.length) % 2 === 0 ? s[s.length/2 -1]+s[s.length/2] : s[Math.floor(s.length/2)];
 }
